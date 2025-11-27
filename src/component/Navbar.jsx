7ba75174c7af44612779a3712fad66348adbc361
Pritom  Majumdar <162822653+rudro33@users.noreply.github.com>
2025-11-28 02:20:05 +0600

    Search

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,64 +1,96 @@
-import Man from "../assets/Man.jpg"
+import React from "react";
 
-export default function Navbar() {
-    const link =(
-        <>
-        <li>Home</li>
-        <li>Tv Show</li>
-        <li>Movie</li>
-        <li>New</li>
-        </>
-    )
+const Navbar = ({ searchText, setSearchText }) => {
+  const link = (
+    <>
+      <li>HOME</li>
+      <li>TV SHOW</li>
+      <li>MOVIES</li>
+      <li>NEW</li>
+    </>
+  );
   return (
-    <div className="navbar bg-black/40 shadow-sm backdrop-blur-md  fixed top-0 left-0 right-0 z-100">
-  <div className='w-11/12 mx-auto flex justify-between items-center'>
+   <div className="navbar bg-black/40 backdrop-blur-md text-white fixed top-0 left-0 right-0 z-100 shadow-md font-montser">
+  <div className="w-11/12 mx-auto flex justify-between items-center">
     <div className="navbar-start">
     <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden text-white">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16"
+          />
+        </svg>
       </div>
+
       <ul
         tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-black/40 backdrop-blur-md rounded-box z-1 mt-3 w-52 p-2 shadow">
+        className="menu menu-sm dropdown-content bg-black/60 backdrop-blur-lg rounded-box z-[1] mt-3 w-52 p-2 shadow text-white"
+      >
         {link}
       </ul>
     </div>
-    <div className='flex items-center gap-10'>
-        <a className="text-xl font-grif text-yellow-400">DRAMATIC</a>
-        <div className="navbar-center hidden lg:flex">
-            <ul className="flex gap-6 ">
-      {link}
-    </ul>
-  </div>
+
+    <div className="flex items-center gap-10">
+      <a className="text-2xl font-semibold text-yellow-400 tracking-wide font-grif">
+        DRAMATIC
+      </a>
+
+      <div className="navbar-center hidden lg:flex">
+        <ul className="flex gap-6 text-white">{link}</ul>
+      </div>
     </div>
   </div>
-  
-    
-  <div className="navbar-end flex gap-6 ">
-    <label className="flex items-center gap-2 bg-white/10 rounded-full px-3 py-2 border-transparent focus-within:border-yellow-400 transition duration-300">
-  <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-    <g
-      strokeLinejoin="round"
-      strokeLinecap="round"
-      strokeWidth="2.5"
-      fill="none"
-      stroke="currentColor"
-    >
-      <circle cx="11" cy="11" r="8"></circle>
-      <path d="m21 21-4.3-4.3"></path>
-    </g>
-  </svg>
-  <input type="search" required placeholder="Search" 
-  className='bg-transparent outline-none text-sm text-white placeholder-white/60 w-24 md:w-40'
-  />
-</label>
-<div className="avatar">
-  <div className="ring-[#facc15] ring-offset-black/40   rounded-full ring-2 ring-offset-2">
-    <img src={Man} className='w-8'/>
+
+  <div className="navbar-end flex gap-6">
+    <label className="flex items-center gap-2 bg-white/10 rounded-full px-3 py-2 border border-transparent focus-within:border-yellow-400 transition duration-300">
+      <svg
+        className="h-[1em] opacity-70"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <g
+          strokeLinejoin="round"
+          strokeLinecap="round"
+          strokeWidth="2.5"
+          fill="none"
+          stroke="currentColor"
+        >
+          <circle cx="11" cy="11" r="8"></circle>
+          <path d="m21 21-4.3-4.3"></path>
+        </g>
+      </svg>
+      <input
+        type="search"
+        value={searchText} 
+        onChange={(e) => setSearchText(e.target.value)}
+        placeholder="Search"
+        className="bg-transparent outline-none text-sm text-white placeholder-white/60 w-24 md:w-40"
+      />
+    </label>
+
+    <div className="avatar">
+      <div className="ring-[#facc15] rounded-full ring-2 ring-offset-2 ring-offset-black/40">
+        <img
+          className="w-8"
+          src="https://i.ibb.co.com/d4nZ7D22/1755511022-918278ab50dceaf47d7ad50ebfe23252.jpg"
+          alt="User"
+        />
+      </div>
+    </div>
+  </div>
   </div>
 </div>
-  </div>
-  </div>
-</div>
-  )
-}
+
+  );
+};
+
+export default Navbar;
